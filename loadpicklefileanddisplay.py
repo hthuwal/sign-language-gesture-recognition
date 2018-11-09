bd22c908cf94b2c49ff979ce7c958709ba54d629
Harish Thuwal <hthuwal@gmail.com>
2018-11-09 18:39:28 +0530

    Port to python3

--- loadpicklefileanddisplay.py
+++ loadpicklefileanddisplay.py
@@ -1,10 +1,12 @@
 import sys
 import pickle
+
 x = pickle.load(open(sys.argv[1], 'rb'))
-print "Length", len(x)
-print x[0]
-print "Press y to print all data and n to exit"
-t = raw_input()
+print("Length", len(x))
+print(x[0])
+print("Press y to print all data and n to exit")
+
+t = input()
 if t == 'y':
     for each in x:
-        print each
+        print(each)
