471651cb60101cbe09d9b7eec314c59bcd9ec53c
Harish Thuwal <hthuwal@gmail.com>
2018-11-11 15:31:19 +0530

    Predict in batches

--- predict_spatial.py
+++ predict_spatial.py
@@ -24,24 +24,28 @@ def load_graph(model_file):
     return graph
 
 
-def read_tensor_from_image_file(file_name, input_height=299, input_width=299, input_mean=0, input_std=255):
+def read_tensor_from_image_file(frames, input_height=299, input_width=299, input_mean=0, input_std=255):
     input_name = "file_reader"
-    file_reader = tf.read_file(file_name, input_name)
-    if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
-    elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
-    elif file_name.endswith(".bmp"):
-        image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-    else:
-        image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
-    float_caster = tf.cast(image_reader, tf.float32)
-    dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    frames = [(tf.read_file(frame, input_name), frame) for frame in frames]
+    decoded_frames = []
+    for frame in frames:
+        file_name = frame[1]
+        file_reader = frame[0]
+        if file_name.endswith(".png"):
+            image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
+        elif file_name.endswith(".gif"):
+            image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
+        elif file_name.endswith(".bmp"):
+            image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+        else:
+            image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
+        decoded_frames.append(image_reader)
+    float_caster = [tf.cast(image_reader, tf.float32) for image_reader in decoded_frames]
+    float_caster = tf.stack(float_caster)
+    resized = tf.image.resize_bilinear(float_caster, [input_height, input_width])
     normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
     sess = tf.Session()
     result = sess.run(normalized)
-
     return result
 
 
@@ -67,11 +71,12 @@ def predict(graph, image_tensor, input_layer, output_layer):
     return results
 
 
-def predict_on_frames(frames_folder, model_file, input_layer, output_layer):
+def predict_on_frames(frames_folder, model_file, input_layer, output_layer, batch_size):
     input_height = 299
     input_width = 299
     input_mean = 0
     input_std = 255
+    batch_size = batch_size
     graph = load_graph(model_file)
 
     labels_in_dir = os.listdir(frames_folder)
@@ -81,12 +86,14 @@ def predict_on_frames(frames_folder, model_file, input_layer, output_layer):
     for each in frames:
         label = each[0]
         print("Predicting on frame of %s\n" % (label))
-        for frame in tqdm(each[2], ascii=True):
+        for i in tqdm(range(0, len(each[2]), batch_size), ascii=True):
+            batch = each[2][i:i + batch_size]
             try:
-                image_path = os.path.join(label, frame)
-                image_tensor = read_tensor_from_image_file(image_path, input_height=input_height, input_width=input_width, input_mean=input_mean, input_std=input_std)
-                pred = predict(graph, image_tensor, input_layer, output_layer)
-                predictions.append([pred, label])
+                batch = [os.path.join(label, frame) for frame in batch]
+                frames_tensors = read_tensor_from_image_file(batch, input_height=input_height, input_width=input_width, input_mean=input_mean, input_std=input_std)
+                pred = predict(graph, frames_tensors, input_layer, output_layer)
+                pred = [[each.tolist(), os.path.basename(label)] for each in pred]
+                predictions.extend(pred)
 
             except KeyboardInterrupt:
                 print("You quit with ctrl+c")
@@ -109,15 +116,15 @@ if __name__ == "__main__":
     parser.add_argument("--input_layer", help="name of input layer", default='Placeholder')
     parser.add_argument("--output_layer", help="name of output layer", default='final_result')
     parser.add_argument('--test', action='store_true', help='Use labeled frames')
+    parser.add_argument("--batch_size", help="batch Size", default=10)
     args = parser.parse_args()
 
     model_file = args.graph
     frames_folder = args.frames_folder
+    input_layer = args.input_layer
+    output_layer = args.output_layer
+    batch_size = int(args.batch_size)
 
-    if args.input_layer:
-        input_layer = args.input_layer
-    if args.output_layer:
-        output_layer = args.output_layer
     if args.test:
         train_or_test = "test"
     else:
@@ -125,9 +132,9 @@ if __name__ == "__main__":
 
     # reduce tf verbosity
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    predictions = predict_on_frames(frames_folder, model_file, input_layer, output_layer)
+    predictions = predict_on_frames(frames_folder, model_file, input_layer, output_layer, batch_size)
 
-    out_file = 'predicted-frames-%s-%s.pkl' % (output_layer, train_or_test)
+    out_file = 'predicted-frames-%s-%s.pkl' % (output_layer.split("/")[-1], train_or_test)
     print("Dumping predictions to: %s" % (out_file))
     with open(out_file, 'wb') as fout:
         pickle.dump(predictions, fout)
